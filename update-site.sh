eed85dc761d40b9bc91b7ac358147f1e75a57f01
Auto Build <build@tst.sh>
2020-05-29 03:55:46 +0000

    Auto Build

--- update-site.sh
+++ update-site.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
+set -o verbose
 set -e
 bundle install
 bundle exec jekyll build
-mv _site ~/recipes_site
+set +e
+rm -r ~/recipes-site
+rm -r ~/flutter-recipes
+set -e
+mv _site ~/recipes-site
+git clone git@github.com:PixelToast/flutter-recipes.git ~/flutter-recipes
+cd ~/flutter-recipes
+rm -r ~/flutter-recipes/*
 git checkout gh-pages
-rm -r ./*
-cp ~/recipes_site/* ./
-rm -r ~/recipes_site
+set +e
+rm -r ~/flutter-recipes/*
+set -e
+cp -r ~/recipes-site/* ./
+git add .
+git commit -m "Auto Build" --author="Auto Build <build@tst.sh>"
+git push -u origin gh-pages
+rm -r ~/recipes-site
 set +e
